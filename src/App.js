395cb83e3796e5e24e5e6348b198fbdd4d81b0e8
pneff <pneff@unicon.net>
2018-09-20 20:08:53 -0700

    39

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Jim', age: 28},
+            {name: 'Paul', age: 27},
+            {name: 'Mary', age: 25}
+        ]
+    }
     render() {
         return (
             <div className="App">
                 <h1>Hi I am a react app</h1>
                 <p>This is really working</p>
-                <Person />
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
             </div>
         );
     }
