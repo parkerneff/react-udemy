25355c2d4488ae3f4dbc96113dccae4fc2ef71e8
pneff <pneff@unicon.net>
2018-09-24 14:03:22 -0700

    95

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 
 import classes from './Person.css'
+import WithClass from "../../../hoc/WithClass";
 
 
 class Person extends Component {
@@ -24,11 +25,11 @@ class Person extends Component {
 render () {
     console.log("[Person.js] inside render", this.props);
     return (
-        <div className={classes.Person} >
+        <WithClass classes={classes.Person} >
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name} />
-        </div>
+        </WithClass>
     )}
 };
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, {PureComponent} from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends PureComponent {
 
@@ -103,14 +104,14 @@ class App extends PureComponent {
 
 
         return (
-            <div className={classes.App}>
+            <WithClass classes={classes.App}>
                 <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
                 <Cockpit appTitle={this.props.title}
                          showPersons={this.state.showPersons}
                          persons={this.state.persons}
                          clicked={this.togglePersonsHandler}/>
                 {persons}
-            </div>
+            </WithClass>
         );
     }
 }
